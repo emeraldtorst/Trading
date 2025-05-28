3b84d50427ddc61c7d0ba88bff43225c0da62fd9
emeraldtorst <emeraldtorstein@gmail.com>
2025-05-28 21:19:16 +0200

    Test: Simplify App.tsx to bare minimum

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <div className="min-h-screen bg-background">
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<div>Hello, this is a test from the router!</div>} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <div>
+    Hello, this is a super basic test!
+  </div>
 );
 
 export default App;
