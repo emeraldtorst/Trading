6154a3d126b32732d28d9ce3db7f69b8eae6cf61
emeraldtorst <emeraldtorstein@gmail.com>
2025-05-28 21:15:49 +0200

    Test: Render simple div in router

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +14,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<div>Hello, this is a test from the router!</div>} />
           </Routes>
         </BrowserRouter>
       </div>
